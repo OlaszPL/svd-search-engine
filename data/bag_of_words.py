fa96848e19efe001cbd5bc0fd6090c324ec6a71f
Aleksander Jóźwik <aleksanderjx@gmail.com>
2025-05-02 00:26:12 +0200

    Update

--- data/bag_of_words.py
+++ data/bag_of_words.py
@@ -14,35 +14,52 @@ from tqdm import tqdm
 # specifies terms as a union of all words from every article, then does preprocessing:
 # 1. stemming
 # 2. stop words removal
-# these two steps will be done when words will be read
+# these two steps will be done when words are be read
 
 # Output -> set of terms
 
+STOP_WORDS = None
+
+def init_worker(stop_words):
+    global STOP_WORDS
+    STOP_WORDS = stop_words
+
+def process_text(text): # for multiple processes
+    ps = PorterStemmer()
+    if text:
+        tokens = word_tokenize(text)
+        return {
+            word for word in (ps.stem(token, to_lowercase = True) for token in tokens) # to lowercase and stem
+            if word not in STOP_WORDS # stop words omitted
+        }
+
+    return set()
+        
 def preprocess(db_path : str, lang : str):
     stop_words = set(stopwords.words(lang)) | set(punctuation)
     conn = sqlite3.connect(db_path)
     terms = set()
 
-    ps = PorterStemmer()
     c = conn.cursor()
-
-    for row in tqdm(c.execute("SELECT text FROM articles")):
-        text = row[0]
-        if text:
-            tokens = word_tokenize(text)
-            terms.update(
-                word for word in (ps.stem(token, to_lowercase = True) for token in tokens) # to lowercase and stem
-                if word not in stop_words # stop words omitted
-            )
-
+    texts = [row[0] for row in c.execute("SELECT text FROM articles")]
     conn.close()
 
+    with concurrent.futures.ProcessPoolExecutor(
+        initializer = init_worker, initargs = (stop_words,)
+    ) as executor:
+        for term_set in tqdm(executor.map(process_text, texts), total = len(texts)):
+            terms.update(term_set)
+
     return terms
+
+
+def main(db_path, lang):
+    terms = preprocess(db_path, lang)
+    print(len(terms))
 
 
 if __name__ == "__main__":
     nltk.download('stopwords')
     nltk.download('punkt_tab')
 
-    terms = preprocess("./wiki.db", "english")
-    print(len(terms))
+    main("./wiki.db", "english")
